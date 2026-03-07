d2b53bce4d2b38a510e928e31c089f5449fc0d8c
ocahavens <ocahavens@gmail.com>
2026-03-07 13:50:05 -0500

    Add project files.

---
+++ SmartCart/App.xaml.cs
@@ -0,0 +1,15 @@
+﻿namespace SmartCart
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+        }
+
+        protected override Window CreateWindow(IActivationState? activationState)
+        {
+            return new Window(new AppShell());
+        }
+    }
+}
