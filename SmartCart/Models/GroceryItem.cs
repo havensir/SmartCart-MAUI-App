0c00f0508279aba956fb0b996e3080a30764b9e9
Melissa Manzon <manzonmj@mail.uc.edu>
2026-03-13 13:39:25 -0400

    These are the files I modified for the project.  Feel free to modify the methods and introduce logic. Thank you

--- SmartCart/Models/GroceryItem.cs
+++ SmartCart/Models/GroceryItem.cs
@@ -8,10 +8,13 @@ namespace SmartCart.Models
 {
     public class GroceryItem
     {
+//<<<<<<< Updated upstream
         public string Name { get; set; } = string.Empty;
         public decimal Price { get; set; }
         public int Quantity { get; set; }
 
         public decimal TotalCost => Price * Quantity;
+//======= Deleted stash changes 
+  
     }
 }
